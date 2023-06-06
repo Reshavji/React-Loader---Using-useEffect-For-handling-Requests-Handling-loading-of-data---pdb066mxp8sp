4c1f7d471bc0c1ce6af93f95f27de3dc37b7cbbb
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-06-06 15:44:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import Loader from "./Loader";
 
@@ -10,68 +10,68 @@ const LoadingStatus = {
 
 const App = () => {
   const BASE_URL = "https://content.newtonschool.co/v1/pr/main/users";
-  const [userId, setUserId] = React.useState(1);
-  const [isLoading, setIsLoading] = React.useState(LoadingStatus.NOT_STARTED);
-  const [userData, setUserData] = React.useState({
+  const [userId, setUserId] = useState(1);
+  const [isLoading, setIsLoading] = useState(LoadingStatus.NOT_STARTED);
+  const [userData, setUserData] = useState({
     id: "",
     email: "",
     name: "",
     phone: "",
-    webiste: "",
+    website: "",
   });
 
-const handleOnClick = () => {
-  setIsLoading(LoadingStatus.IN_PROGRESS); // Start loading
+  const handleOnClick = () => {
+    setIsLoading(LoadingStatus.IN_PROGRESS); // Start loading
 
-  // Simulate a delay of 2 seconds
-  setTimeout(() => {
-    fetch(`${BASE_URL}/${userId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setIsLoading(LoadingStatus.SUCCESS); // Loading complete
-        setUserData(data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }, 2000);
-};
-
+    // Simulate a delay of 2 seconds
+    setTimeout(() => {
+      fetch(`${BASE_URL}/${userId}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setIsLoading(LoadingStatus.SUCCESS); // Loading complete
+          setUserData(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }, 2000);
+  };
 
   const onChangeHandler = (event) => {
     setUserId(event.target.value);
   };
 
- return (
-  <div id="main">
-    <label htmlFor="number">Enter an id for the user between 1 to 100</label>
-    <input
-      type="number"
-      value={userId}
-      onChange={onChangeHandler}
-      id="input"
-      min={1}
-      max={10}
-    />
-    <button id="btn" onClick={handleOnClick}>
-      Get User
-    </button>
+  return (
+    <div id="main">
+      <label htmlFor="number">Enter an id for the user between 1 to 100</label>
+      <input
+        type="number"
+        value={userId}
+        onChange={onChangeHandler}
+        id="input"
+        min={1}
+        max={10}
+      />
+      <button id="btn" onClick={handleOnClick}>
+        Get User
+      </button>
 
-    <div id="data">
-      {isLoading === LoadingStatus.IN_PROGRESS ? (
-        <Loader /> // Display Loader component while loading
-      ) : (
-        <>
-          <h1>Click on the button to get the user</h1>
-          <h4 id="id">{userData.id}</h4>
-          <h4 id="email">{userData.email}</h4>
-          <h4 id="name">{userData.name}</h4>
-          <h4 id="phone">{userData.phone}</h4>
-          <h4 id="website">{userData.website}</h4>
-        </>
-      )}
+      <div id="data">
+        {isLoading === LoadingStatus.IN_PROGRESS ? (
+          <Loader /> // Display Loader component while loading
+        ) : (
+          <>
+            <h1>Click on the button to get the user</h1>
+            <h4 id="id">{userData.id}</h4>
+            <h4 id="email">{userData.email}</h4>
+            <h4 id="name">{userData.name}</h4>
+            <h4 id="phone">{userData.phone}</h4>
+            <h4 id="website">{userData.website}</h4>
+          </>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
